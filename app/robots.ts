563e6a23e3d6ee297e0aad4067066aa32f954ed8
Seungwoo321 <seungwoo321@gmail.com>
2025-07-25 22:15:33 +0900

    revert: sitemap URL을 원래대로 복원 (www 제거)

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/private/'],
     },
-    sitemap: 'https://www.toolypet.com/sitemap.xml',
+    sitemap: 'https://toolypet.com/sitemap.xml',
   }
 }
